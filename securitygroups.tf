17552e0bc0b9e60148c4b679f1ac0827134ca958
Accurics <admin@accurics.com>
2020-10-02 03:08:12 +0000

    Auto Remediation By Accurics

--- securitygroups.tf
+++ securitygroups.tf
@@ -6,13 +6,10 @@ resource "aws_security_group" "public_internet" {
 
   # HTTP access from anywhere
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-      #"10.0.0.0/16"
-    ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["<cidr>"]
   }
   egress {
     from_port   = 0
@@ -29,22 +26,16 @@ resource "aws_security_group" "web" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-      #"10.0.0.0/16"
-    ]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["<cidr>"]
   }
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-      #"10.0.0.0/16"
-    ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["<cidr>"]
   }
   egress {
     from_port = 0
